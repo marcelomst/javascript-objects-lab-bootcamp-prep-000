ecaf406f669bdeaaf42f4aa0a1148e8ec1cea5b3
Marcelo Martinez <marcelomst1@gmail.com>
2019-12-28 19:15:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ function updateObjectWithKeyAndValue(object, key, value){
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
    return Object.assign(object,{[key]: value});
 }
+function deleteFromObjectByKey(object, key){
+  newObj = Object.assign({},object)
+  delete newObject[[key]]
+  return newObj 
+}
