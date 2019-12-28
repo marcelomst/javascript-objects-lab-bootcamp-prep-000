8c03baf292f7a6610f36c9fa40424caddc89918a
Marcelo Martinez <marcelomst1@gmail.com>
2019-12-28 18:38:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-function Good luck!
-
-HINT: You might find deleteFromObjectByKey to be a bit hard to write non-destructively. Think about how we learned to use Object.assign. What happens if we do
-
+function updateObjectWithKeyAndValue(object, key, value){
+  
+}
